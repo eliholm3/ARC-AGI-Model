dee56cc9be589fc7168e9dcf8d1ed20281f0e7be
BanTheMan <brandonmgomes06@gmail.com>
2025-11-14 22:02:48 -0600

    oops

--- src/data_pipeline/mask_gen.py
+++ src/data_pipeline/mask_gen.py
@@ -6,11 +6,11 @@ def generate_valid_mask(
         W,  # data width
         original_size
 ):
-    h_0, w_o = original_size
+    h_0, w_0 = original_size
 
     # Row valid mask: True for rows < h0
     row_mask = torch.arange(H).unsqueeze(1) < h_0   # (H, 1)
-    
+
     # Col valid mask: True for cols < w0
     col_mask = torch.arange(W).unsqueeze(0) < w_0   # (1, W)
 
